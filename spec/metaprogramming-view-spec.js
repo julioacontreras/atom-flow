3efe032ae34119e300f5e3615ca2d42205d719ab
Julio Contreras <46874081+julioacontreras@users.noreply.github.com>
2019-04-08 15:51:18 +0200

    first commit

---
+++ spec/metaprogramming-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MetaprogrammingView from '../lib/metaprogramming-view';
+
+describe('MetaprogrammingView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
